a432923cd34fa19a23c8ce96799077801ba61c14
Ashutosh <1ashutoshsahu@gmail>
2023-10-28 16:33:29 +0530

    Show all Running or Ended TV shows in home page.

--- app/src/main/java/com/example/tvshows/activities/MainActivity.java
+++ app/src/main/java/com/example/tvshows/activities/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.tvshows.activities;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.tvshows.adapters.TVShowAdapter;
 import com.example.tvshows.R;
+import com.example.tvshows.adapters.TVShowAdapter;
+import com.example.tvshows.databinding.ActivityMainBinding;
 import com.example.tvshows.models.TVShow;
 import com.example.tvshows.viewmodels.MostPopularTVShowsViewModel;
-import com.example.tvshows.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,10 +22,10 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding activityMainBinding;
     private MostPopularTVShowsViewModel viewModel;
-    private List<TVShow> tvShows = new ArrayList<>();
+    private final List<TVShow> tvShows = new ArrayList<>();
     private TVShowAdapter tvShowAdapter;
-   
-
+    private int currentPage = 1;
+    private int totalAvailablePages = 1;
 
 
     @Override
@@ -38,26 +41,47 @@ public class MainActivity extends AppCompatActivity {
         viewModel = new ViewModelProvider(this).get(MostPopularTVShowsViewModel.class);
         tvShowAdapter = new TVShowAdapter(tvShows);
         activityMainBinding.rvTvShows.setAdapter(tvShowAdapter);
-
-
+        activityMainBinding.rvTvShows.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (!activityMainBinding.rvTvShows.canScrollVertically(1)) {
+                    if (currentPage <= totalAvailablePages) {
+                        currentPage += 1;
+                        getMostPopularTVShows();
+                    }
+                }
+            }
+        });
         getMostPopularTVShows();
     }
 
 
     @SuppressLint("NotifyDataSetChanged")
     private void getMostPopularTVShows() {
-        activityMainBinding.setIsLoading(true);
-        viewModel.getMostPopularTVShows(0).observe(this, mostPopularTVShowsResponse -> {
-
-            activityMainBinding.setIsLoading(false);
-            if (mostPopularTVShowsResponse!=null){
-                if (mostPopularTVShowsResponse.getTvShows()!=null){
+//        activityMainBinding.setIsLoading(true);
+        toggleLoading();
+        viewModel.getMostPopularTVShows(currentPage).observe(this, mostPopularTVShowsResponse -> {
+            toggleLoading();
+//            activityMainBinding.setIsLoading(false);
+            if (mostPopularTVShowsResponse != null) {
+                totalAvailablePages = mostPopularTVShowsResponse.getTotalPages();
+                if (mostPopularTVShowsResponse.getTvShows() != null) {
+                    int oldCount = tvShows.size();
                     tvShows.addAll(mostPopularTVShowsResponse.getTvShows());
-                    tvShowAdapter.notifyDataSetChanged();
+                    tvShowAdapter.notifyItemRangeInserted(oldCount, tvShows.size());
+//                    tvShowAdapter.notifyDataSetChanged();
                 }
             }
 
         });
     }
 
+    private void toggleLoading() {
+        if (currentPage == 1) {
+            activityMainBinding.setIsLoading(activityMainBinding.getIsLoading() == null || !activityMainBinding.getIsLoading());
+        } else {
+            activityMainBinding.setIsLoadingMore(activityMainBinding.getIsLoadingMore() == null || !activityMainBinding.getIsLoadingMore());
+        }
+    }
 }
